7d4b5f86207c361c827fc3611e5732e4739f0f5a
andreadldev <andrea.delisi2@gmail.com>
2023-04-03 15:24:26 +0200

    metodi testcase

---
+++ Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace net_calculator_tester
+{
+    public static class Calculator
+    {
+        public static float Add(float num1, float num2) => num1 + num2;
+
+        public static float Subtract(float num1, float num2) => num1 - num2;
+
+        public static float Multiply(float num1, float num2) => num1 * num2;
+
+        public static float Divide(float num1, float num2) 
+        {
+            if (num2 == 0)
+            {
+                throw new Exception("Non puoi dividere per 0");
+            }
+            return num1 / num2;
+        }
+    }
+}
